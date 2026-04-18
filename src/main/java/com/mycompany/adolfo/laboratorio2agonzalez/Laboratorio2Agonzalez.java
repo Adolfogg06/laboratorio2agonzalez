1d8b862a9156e76f6a74500d88051fe0d2df758b
adolf <adolf@Adolfo_PC>
2026-04-18 12:52:57 -0600

    Agregando el proyecto a la nube

---
+++ src/main/java/com/mycompany/adolfo/laboratorio2agonzalez/Laboratorio2Agonzalez.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.adolfo.laboratorio2agonzalez;
+import com.mycompany.adolfo.laboratorio2agonzalez.view.login;
+/**
+ *
+ * @author adolf
+ */
+public class Laboratorio2Agonzalez {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        login lg = new login();
+        lg.setVisible(true);
+    }
+}
